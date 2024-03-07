f442296c7e505fd7a6e467fd65b683e0cfe57c9a
Xeroas <jacktaghd@gmail.com>
2024-03-07 04:06:34 +0000

    fixed XML Comment typos

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -25,7 +25,7 @@ namespace CMP1903_A1_2324
             Game Game1 = new Game(); //Object instantiation, loading "Game" into the program class.
 
             Testing Test1 = new Testing(); //Object instantiation, loading "Testing" into the program class.
-            Test1.Test(); //Run the testing method
+            Test1.Test(); //Run the testing method.
             Console.WriteLine("-------------------\nTesting complete\n-------------------"); //So long as no errors occur, this line will be output to let the user know testing has completed.
 
 
@@ -35,30 +35,30 @@ namespace CMP1903_A1_2324
                 Console.WriteLine("If you would like to stop rolling please enter 'Stop'\nPlease enter how many times would you like to roll the dies :");
                 userInput = Console.ReadLine();
 
-                if (userInput.ToLower() == "stop") //If user enters "stop", stop the loop
+                if (userInput.ToLower() == "stop") //If user enters "stop", stop the loop.
                 {
                     break;
                 }
 
                 try
                 {
-                    int timesToRoll = Int32.Parse(userInput); //Cast userInput to int
-                    for (int rollCounter = 1; rollCounter <= timesToRoll; rollCounter++) //Create counter to Roll dies based on user input
+                    int timesToRoll = Int32.Parse(userInput); //Cast userInput to int.
+                    for (int rollCounter = 1; rollCounter <= timesToRoll; rollCounter++) //Create counter to Roll dies based on user input.
                     {
-                        var gameStart1 = Game1.StartGame(); //Run the "Game" method (Roll three die)
+                        var gameStart1 = Game1.StartGame(); //Run the "Game" method (Roll three die).
                         Console.WriteLine($"-------------------\nSum is = {gameStart1.Item1}\n-------------------"); ///Output the sum of the three dies (With some ASCII formatting).
                     }
                 }
-                catch (FormatException) //If user inputs string this will catch it and display appropriate message
+                catch (FormatException) //If user inputs a string this will catch it and display appropriate message.
                 {
                     Console.WriteLine("\nIncorrect input, please enter a number!\n");
                 }
 
-                catch (Exception) //If user error-causing input this will catch it and display appropriate message
+                catch (Exception) //If user enters error-causing input this will catch it and display appropriate message.
                 {
                     Console.WriteLine("Unexpected error, please try again!");
                 }
-            } while (true); //Ensure code above loops while true (user has not told the code to stop)
+            } while (true); //Ensure the code above loops while true (user has not told the code to stop).
         }
     }
 }
